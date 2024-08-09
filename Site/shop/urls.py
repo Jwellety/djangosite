96ed9b7c8133be6d2f70854d38303e0134fdf03f
monsoon22 <minorviva@gmail.com>
2024-08-10 02:20:22 +0530

    struct2

--- Site/shop/urls.py
+++ Site/shop/urls.py
@@ -6,6 +6,14 @@ urlpatterns = [
     path('', views.index, name="Home"),
     path('aboutus', views.aboutus, name = 'aboutus'),
     path('signup', views.signup, name = 'signup'),
-    path('login', views.login, name = 'login')
-
+    path('community', views.community, name = 'community'),
+    path('event', views.event, name = 'event'),
+    path('faq', views.faq, name = 'faq'),
+    path('help', views.help, name = 'help'),
+    path('login', views.login, name = 'login'),
+    path('pricing', views.pricing, name = 'pricing'),
+    path('privatepolicy', views.privatepolicy, name = 'privatepolicy'),
+    path('product', views.product, name = 'product'),
+    path('service', views.service, name = 'service'),
+    path('termandcondition', views.termandcondition, name = 'termandcondition'),
 ]
--- Site/shop/views.py
+++ Site/shop/views.py
@@ -6,9 +6,26 @@ def index(request):
     return render(request,'shop/home.html')
 def aboutus(request):
     return render(request,'shop/aboutus.html')
-
-
 def signup(request):
     return render(request,'shop/signup.html')
 def login(request):
     return render(request,'shop/login.html')
+def community(request):
+    return render(request,'shop/community.html')
+def event(request):
+    return render(request,'shop/event.html')
+def faq(request):
+    return render(request,'shop/faq.html')
+def help(request):
+    return render(request,'shop/help.html')
+def pricing(request):
+    return render(request,'shop/pricing.html')
+def privatepolicy(request):
+    return render(request,'shop/privatepolicy.html')
+def product(request):
+    return render(request,'shop/product.html')
+def service(request):
+    return render(request,'shop/service.html')
+def termandcondition(request):
+    return render(request,'shop/termandcondition.html')
+
